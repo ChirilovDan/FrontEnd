8f0de9ce2a770dc7924a274f0d1568c87bd9a475
Chirilov Dan <dchirilov2001@gmail.com>
2023-09-16 04:40:05 +0300

    added lesson with switch

---
+++ JS/JavaScript_Fundamentals/Lesson_011_Switch/Tasks/Task1/script.js
@@ -0,0 +1,7 @@
+let browser=prompt("Write a browser:");
+
+if(browser=="Edge"){
+	alert( "You've got the Edge!" );
+}else if(browser=="Edge"|| browser=="Firefox"|| browser=="Safari"||browser=="Opera"){
+	alert('Okay we support these browsers too');
+}else alert( 'We hope that this page looks ok!' );
