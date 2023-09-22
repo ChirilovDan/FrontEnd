62c05f607941041605a6063871bf689e19681dcc
Chirilov Dan <dchirilov2001@gmail.com>
2023-09-22 23:46:06 +0300

    Smth to commit but is not completly made

---
+++ JS/Problmes/Calculator/script.js
@@ -0,0 +1,33 @@
+
+
+
+	let nr1=+prompt("Give nr1=");
+	let nr2=+prompt("Give nr2=");
+	do{
+
+	let operant=prompt("Operant:")
+	let result=0;
+	switch(operant){
+		case "+":
+		result=result+ add(nr1,nr2);
+		return result;
+		
+	}
+}
+while(operand==="C")
+
+function add(nr1,nr2){
+	return nr1+nr;
+}
+
+function deleteF(nr1,nr2){
+	return nr1-nr;
+}
+
+function multiple(nr1,nr2){
+	return nr1*nr;
+}
+
+function deleteF(nr1,nr2){
+	return nr1-n2;
+}
