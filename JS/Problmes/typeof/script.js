c74b74c65f8bd693bc2ad4831fe354c872ce5b44
Chirilov Dan <dchirilov2001@gmail.com>
2023-10-05 03:56:08 +0300

    solved problem

--- JS/Problmes/typeof/script.js
+++ JS/Problmes/typeof/script.js
@@ -3,6 +3,6 @@ let array=['book',2.21,10,true,[],new Date()];
 //alert(typeof(array[2]));
 
 for(let i=0; i<array.length;i++){
-	if(typeof(array[i])==="number")
+	if(typeof(array[i])==="number" && Number.isInteger(array[i]))
 		console.log(array[i]);
 }
